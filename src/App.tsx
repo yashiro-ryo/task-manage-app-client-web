31bf8b22f276bab0c80f1b5e2858497a54dfd549
yashiroryo <>
2023-01-17 05:45:09 +0900

    feat: serverから取得したタスクが表示されるように取得タイミングを調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
+import { io } from "socket.io-client";
+
+const socket = io("http://localhost:5050");
 
 function App() {
+  const [groups, setGroups] = useState([]);
+  socket.on("connect", () => {
+    console.log("socket connected");
+  });
+  socket.on("init-tasks", (data: any) => {
+    console.log(data);
+    setGroups(data);
+  });
   return (
     <>
       <Navbar />
       <BrowserRouter>
         <Routes>
-          <Route path="/home/:domainId/:projectId" element={<Home />} />
+          <Route
+            path="/home/:domainId/:projectId"
+            element={<Home groups={groups} />}
+          />
         </Routes>
       </BrowserRouter>
     </>
