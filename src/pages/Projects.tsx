1f63cbbd3840add8cb023651d5d1d77a5674f4b6
yashiroryo <>
2023-02-08 17:42:34 +0900

    fix: projectページのスタイルをtaskのページと共通にするように修正

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,7 +3,22 @@ import ProjectCard from "../components/ProjectCard";
 import styled from "styled-components";
 
 const StyledPage = styled.div`
-  padding: 10px;
+  width: 100%;
+  height: calc(100vh - 50px);
+  background-color: #e7e7e7;
+  color: #000000;
+  padding: 15px;
+  flex-wrap: wrap;
+  overflow-x: auto;
+`;
+
+const PageTitle = styled.p`
+  font-size: 20px;
+`;
+
+const ProjectList = styled.div`
+  display: flex;
+  align-items: flex-start;
 `;
 
 export default function Project() {
@@ -21,10 +36,12 @@ export default function Project() {
   return (
     <>
       <StyledPage>
-        <p>プロジェクト一覧</p>
-        {projects.map((project, index) => {
-          return <ProjectCard project={project} key={`project-${index}`} />;
-        })}
+        <PageTitle>プロジェクト一覧</PageTitle>
+        <ProjectList>
+          {projects.map((project, index) => {
+            return <ProjectCard project={project} key={`project-${index}`} />;
+          })}
+        </ProjectList>
       </StyledPage>
     </>
   );
