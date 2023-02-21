fdced37f13d7489cd04c5a7c43c1b99b91b94856
yashiroryo <>
2023-02-22 00:33:25 +0900

    feat: signoutのapiを呼ぶ処理を追加

--- src/pages/Signout.tsx
+++ src/pages/Signout.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container } from "react-bootstrap";
+import { logout } from '../etc/logout'
 
 export default function Signout() {
+  let setupPrepaed = false;
+  useEffect(() => {
+    if (!setupPrepaed) {
+      logout()
+      setupPrepaed = true;
+    }
+  });
   return (
     <Container>
       <p>ログアウトしました。</p>
