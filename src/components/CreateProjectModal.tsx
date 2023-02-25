39505c668e679f5cea371cc3e2778d304bbdc480
yashiroryo <>
2023-02-25 09:59:39 +0900

    feat: project作成apiを呼ぶように実装

--- src/components/CreateProjectModal.tsx
+++ src/components/CreateProjectModal.tsx
@@ -1,5 +1,7 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
 import { Modal, Button, Form } from "react-bootstrap";
+import url from "../etc/url";
 
 type Props = {
   isVisible: boolean;
@@ -7,16 +9,53 @@ type Props = {
 };
 
 export default function CreateProjectModal(props: Props) {
+  const [inputText, setInputText] = useState("");
+  const [errorText, setErrorText] = useState("");
   const handleClose = () => {
     props.setVisible(false);
+    clearText();
   };
+
+  const clearText = () => {
+    setInputText("");
+    setErrorText("");
+  };
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value);
+  };
+
+  const doCreateProject = () => {
+    axios
+      .post(url.getServerApi(process.env.NODE_ENV) + "/api/v1/project", {
+        projectName: inputText,
+      })
+      .then((data: any) => {
+        if (data.data.hasError) {
+          setErrorText(data.data.errorMsg);
+        } else {
+          handleClose();
+          // 更新
+        }
+      })
+      .catch((e) => {
+        console.error(e);
+        handleClose();
+      });
+  };
+
   return (
     <Modal show={props.isVisible} onHide={handleClose}>
       <Modal.Body>
         <Form>
           <Form.Group className="mb-3">
             <Form.Label>プロジェクト名</Form.Label>
-            <Form.Control type="text" placeholder="プロジェクト名" />
+            <Form.Control
+              type="text"
+              placeholder="プロジェクト名"
+              onChange={onChange}
+            />
+            <p>{errorText}</p>
           </Form.Group>
         </Form>
       </Modal.Body>
@@ -24,7 +63,7 @@ export default function CreateProjectModal(props: Props) {
         <Button variant="secondary" onClick={handleClose}>
           キャンセル
         </Button>
-        <Button variant="primary" onClick={handleClose}>
+        <Button variant="primary" onClick={doCreateProject}>
           プロジェクト作成
         </Button>
       </Modal.Footer>
